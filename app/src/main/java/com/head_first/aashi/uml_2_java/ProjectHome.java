fba12c745b4d430f43baf92ece4325c2e9e8d0f5
Moses Ullagi <umosesdavid@gmail.com>
2016-09-22 21:03:56 +1200

    Updated the needed comments for my user stories

--- app/src/main/java/com/head_first/aashi/uml_2_java/ProjectHome.java
+++ app/src/main/java/com/head_first/aashi/uml_2_java/ProjectHome.java
@@ -10,6 +10,8 @@ import project.Project;
 
 
 /**
+ * This will be the main display page when the user starts the application
+ * User will have the option to start a new UML project or load a previously saved project
  * Created by moses on 18/09/2016.
  */
 public class ProjectHome extends AppCompatActivity {
@@ -24,6 +26,7 @@ public class ProjectHome extends AppCompatActivity {
 
     }
 
+   //Handle the Start Project Button
    protected void onStartProject(View v){
        if(v.getId()==R.id.startProject){
            Intent projectNew = new Intent(this, ProjectPage.class);
@@ -31,10 +34,16 @@ public class ProjectHome extends AppCompatActivity {
        }
    }
 
+    //Handles the Open Project Button
     protected void onOpenProject(View v){
-        if(v.getId()==R.id.startProject){
-            Intent projectNew = new Intent(this, ProjectPage.class);
-            startActivity(projectNew);
+        if(v.getId()==R.id.openProject){
+            //We can read the file as Bytes from a String or use Gson and shared preference to read the file as a UML Project object.
+            //This feature is not part of Sprint0 hence the methods will be discussed with the Scrum Master & the team before finalising
+
+//            Method to open file using Gson
+//            Gson gson = new Gson();
+//            String json = mPrefs.getString("MyObjectName", "");
+//            Project object = gson.fromJson(json, Project.class);   This Class has already been defined under main->src
         }
     }
 
--- app/src/main/java/com/head_first/aashi/uml_2_java/ProjectPage.java
+++ app/src/main/java/com/head_first/aashi/uml_2_java/ProjectPage.java
@@ -16,6 +16,7 @@ import java.io.ObjectOutputStream;
 import project.Project;
 
 /**
+ * This will be the project page where the user can start adding classes and edit/save changes
  * Created by moses on 18/09/2016.
  */
 public class ProjectPage extends Activity {
@@ -32,17 +33,33 @@ public class ProjectPage extends Activity {
 
 
 
+    //Handles the Save Project Button
+    //All information from UML classes will be saved onto the internal storage of the device as a String File.
+    //Alternatively we can use Gson to store the UML data as a Project data. (This option is to discussed in the next Sprint)
+    //I have already added the appropriate library file to the build.gradle in the dependencies section.
     protected void onSaveProject(View v){
 
         if(v.getId()==R.id.saveProject){
+
+            //Update the project name from the Project Name Text Field
             p.setProjectName(projectName.getText().toString());
+
             try {
                 FileOutputStream fileOut = openFileOutput(p.getProjectName(),MODE_PRIVATE);
-                fileOut.write(p.toString().getBytes());
+                fileOut.write(p.toString().getBytes());  //Store the toString() of the project object.
                 Toast.makeText(getApplicationContext(),"Project Saved", Toast.LENGTH_LONG).show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+//            Other Method
+//            SharedPreferences myPreferns = getPreferences(MODE_PRIVATE);
+//
+//            Editor prefsEditor = myPreferns.edit();
+//            Gson gson = new Gson();
+//            String json = gson.toJson(myObject); // myObject
+//            prefsEditor.putString("MyObject", json);
+//            prefsEditor.commit();
 
         }
     }
